818af4eeda9c3b32b76aa0d145ef157bedc59222
tobakonek1 <tobakonek1@gmail.com>
2023-08-28 16:35:52 +0700

    create news2 services2

---
+++ template/vue_components/news2.vue
@@ -0,0 +1,218 @@
+<template>
+  <div class="news2">
+    <div class="news2-title">TIN TỨC & SỰ KIỆN</div>
+    <div class="swiper-container" id="news-swiper-id">
+      <div class="swiper-wrapper">
+        <div
+          v-for="(item, index) in news"
+          :key="index"
+          class="news-card swiper-slide"
+        >
+          <a class="news-link" :href="item.link">
+            <div class="img">
+                <img :src="item.imageSrc" alt="" />
+            </div>
+            <div class="news-title">{{ item.title }}</div>
+          </a>
+          <div class="news-content">{{ item.description }}</div>
+        </div>
+      </div>
+      <div class="swiper-navigation-custom">
+        <div
+          class="swiper-button-prev"
+          id="news-swiper-prev"
+          aria-controls="swiper-wrapper"
+        ></div>
+        <div class="swiper-counting-paginate">
+          <div class="index"></div>
+          <div>/</div>
+          <div class="slides"></div>
+        </div>
+        <div
+          class="swiper-button-next"
+          id="news-swiper-next"
+          aria-controls="swiper-wrapper"
+        ></div>
+      </div>
+      <div
+        class="swiper-pagination"
+      ></div>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  data: function () {
+    return {
+      news: [
+        {
+          imageSrc: "../template/images/title-1682585524.jpg",
+          title: "Tầm quan trọng của công tác đào tạo nhân viên bảo vệ",
+          link: "https://truongthanhdaiviet.com.vn/tin-tuc/tam-quan-trong-cua-cong-tac-dao-tao-nhan-vien-bao-ve-39",
+          description: "Công tác đào tạo tại Trường Thành Đại Việt",
+        },
+        {
+          imageSrc: "../template/images/title-1682306936.jpg",
+          title: "Lịch nghỉ lễ giỗ tổ Hùng Vương, 30/04 và 01/05",
+          link: "https://truongthanhdaiviet.com.vn/tin-tuc/lich-nghi-le-gio-to-hung-vuong-3004-va-0105-38",
+          description:
+            "Công ty cổ phần dịch vụ bảo vệ Trường Thành Đại Việt thông báo lịch nghỉ lễ giỗ tổ Hùng Vương, 30/04 và 01/05",
+        },
+        {
+          imageSrc: "../template/images/title-1678347423.png",
+          title: "Trường Thành Đại Việt bảo vệ sự kiện Ecopark",
+          link: "https://truongthanhdaiviet.com.vn/tin-tuc/truong-thanh-dai-viet-bao-ve-su-kien-ecopark-35",
+          description:
+            "Ngày 6/3/2023 công ty Trường Thành Đại Việt đã thực hiện nhiêm vụ bảo vệ an ninh tại sự kiện Ecopark Hưng Yên.",
+        },
+        {
+          imageSrc: "../template/images/title-1678691451.png",
+          title: "Phụ nữ với nghề bảo vệ",
+          link: "https://truongthanhdaiviet.com.vn/tin-tuc/phu-nu-voi-nghe-bao-ve-36",
+          description:
+            "Công ty cổ phần dịch vụ bảo vệ Trường Thành Đại Việt chúc mừng ngày Quốc tế phụ nữ",
+        },
+        {
+          imageSrc: "../template/images/title-1677489241.png",
+          title: "Công ty Trường Thành Đại Việt đào tạo phòng cháy chữa cháy",
+          link: "https://truongthanhdaiviet.com.vn/tin-tuc/cong-ty-truong-thanh-dai-viet-dao-tao-phong-chay-chua-chay-24",
+          description:
+            "Phòng nghiệp vụ mở lớp đào tạo phòng cháy chữa cháy cho nhân viên",
+        },
+        {
+          imageSrc: "../template/images/title-1677550364.png",
+          title: "Trường Thành Đại Việt chúc Tết nhân viên bảo vệ",
+          link: "https://truongthanhdaiviet.com.vn/tin-tuc/truong-thanh-dai-viet-chuc-tet-nhan-vien-bao-ve-25",
+          description:
+            "Ban lãnh đạo và phòng Nghiệp vụ công ty Trường Thành Đại Việt đã đi chúc Tết và lì xì cho nhân viên",
+        },
+        {
+          imageSrc: "../template/images/title-1677552235.png",
+          title: "Lễ Tổng Kết năm 2022 của công ty Trường Thành Đại Việt",
+          link: "https://truongthanhdaiviet.com.vn/tin-tuc/le-tong-ket-nam-2022-cua-cong-ty-truong-thanh-dai-viet-28",
+          description:
+            "Công ty Trường Thành Đại Việt tổ chức lễ tổng kết năm 2022 và kế hoạch phát triển năm 2023",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.news2 {
+    background-image: url(../images/bg-blogevents.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top center;
+    padding: 90px 0;
+}
+.swiper-container {
+    max-width: 1200px;
+    margin: auto;
+    overflow: hidden;
+}
+.news2-title {
+    font-weight: 900;
+    font-size: 32px;
+    text-align: center;
+    color: #1d5a99;
+    text-transform: uppercase;
+    margin-bottom: 24px;
+}
+.swiper-slide {
+    width: 30%;
+    gap: 5%;
+}
+.news-link {
+    
+}
+.news-link .img {
+    height: 280px;
+    overflow: hidden;
+    display: inline-block;
+}
+.news-link img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+.news-link .news-title {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    font-weight: bold;
+    font-size: 20px;
+    letter-spacing: -0.01em;
+    color: #1d5a99;
+    margin-bottom: 6px;
+}
+.news-link .news-content {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    font-weight: 400;
+    font-size: 16px;
+    color: #666;
+    margin-bottom: 0;
+}
+.news-link:hover img {
+    transition: all .3s linear;
+    transform: scale(1.1);
+    transform-origin: center center;
+    filter: opacity(0.5);
+}
+.swiper-pagination {
+    display: none;
+}
+.swiper-navigation-custom {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  margin-top: 60px;
+}
+.swiper-navigation-custom .swiper-counting-paginate {
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+.swiper-navigation-custom .swiper-counting-paginate > * {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1;
+  color: #333;
+}
+#news-swiper-next, #news-swiper-prev {
+    margin-top: 0;
+    position: relative;
+    width: 40px;
+    height: 40px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background: #1d5a99;
+    border: 2px solid #fff;
+    box-shadow: 0px 2px 10px rgba(0,0,0,.25);
+    border-radius: 50%;
+}
+#news-swiper-next::after, #news-swiper-prev::after {
+    flex-shrink: 0;
+    color: #fff;
+    font-weight: bold;
+    font-size: 12px;
+}
+@media screen and (max-width: 400px) {
+    .news2 {
+        background-image: url(../images/bg-blogevents-mobile.png) !important;
+    }
+    .swiper-container {
+        padding: 0 24px !important;
+    }
+    .swiper-navigation-custom {
+        margin-top: 0 !important;
+    }
+}
+</style>
---
+++ template/vue_components/services2.vue
@@ -0,0 +1,260 @@
+<template>
+  <div class="services2">
+    <h1 class="services2-title">
+      DỊCH VỤ CỦA CHÚNG TÔI
+    </h1>
+    <div class="swiper-container" id="services-swiper-id">
+      <div class="swiper-wrapper">
+        <div v-for="(item, index) in services" :key="index" class="swiper-slide service-item-swiper">
+          <div class="img-shadow"></div>
+          <img :src="item.imageSrc" alt="Image 1" />
+          <h2>{{ item.title }}</h2>
+          <div class="service-hover">
+            <div class="hover-title">{{ item.title }}</div>
+            <div class="hover-button">
+              <a :href="item.link"
+                >Xem chi tiết
+                <svg
+                  width="13"
+                  height="12"
+                  viewBox="0 0 13 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M6.5 0L5.4425 1.0575L9.6275 5.25H0.5V6.75H9.6275L5.4425 10.9425L6.5 12L12.5 6L6.5 0Z"
+                    fill="#1D5A99"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="swiper-button-next"
+        id="services-swiper-next"
+        aria-controls="swiper-wrapper"
+      ></div>
+      <div
+        class="swiper-button-prev"
+        id="services-swiper-prev"
+        aria-controls="swiper-wrapper"
+      ></div>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  data: function () {
+    return {
+      services: [
+        {
+          imageSrc: '../template/images/title-home-1678693484.png',
+          title: 'Bảo vệ công trường',
+          link: 'https://truongthanhdaiviet.com.vn/dich-vu/bao-ve-cong-truong-14'
+        },
+        {
+          imageSrc: '../template/images/title-home-1677510758.png',
+          title: 'Bảo vệ nhà máy, xí nghiệp',
+          link: 'https://truongthanhdaiviet.com.vn/dich-vu/bao-ve-nha-may-xi-nghiep-8'
+        },
+        {
+          imageSrc: '../template/images/title-home-1677491114.png',
+          title: 'Bảo vệ tòa nhà văn phòng, chung cư',
+          link: 'https://truongthanhdaiviet.com.vn/dich-vu/bao-ve-toa-nha-van-phong-chung-cu-7'
+        },
+        {
+          imageSrc: '../template/images/title-home-1677506995.jpg',
+          title: 'Bảo vệ siêu thị, trung tâm thương mại, cửa hàng.',
+          link: 'https://truongthanhdaiviet.com.vn/dich-vu/bao-ve-sieu-thi-trung-tam-thuong-mai-cua-hang-5'
+        },
+        {
+          imageSrc: '../template/images/title-home-1677556782.jpg',
+          title: 'Bảo vệ yếu nhân, ca sỹ, nghệ sỹ, ngôi sao',
+          link: 'https://truongthanhdaiviet.com.vn/dich-vu/bao-ve-yeu-nhan-ca-sy-nghe-sy-ngoi-sao-4'
+        },
+        {
+          imageSrc: '../template/images/title-home-1677551663.jpg',
+          title: 'Bảo vệ Khu nghỉ mát, khách sạn',
+          link: 'https://truongthanhdaiviet.com.vn/dich-vu/bao-ve-khu-nghi-mat-khach-san-3'
+        },
+      ]
+    };
+  },
+};
+</script>
+<style scoped>
+.services2 {
+  background-image: url(../images/bg-ourservices.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top center;
+  padding: 60px 0 0;
+}
+.services2-title {
+  color: white;
+  text-align: center;
+  font-weight: bold;
+}
+.swiper-container {
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.swiper-slide {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 25% !important;
+  height: 679px;
+}
+@media screen and (max-width: 400px) {
+  .swiper-slide {
+    width: 100% !important;
+  }
+  .services2 {
+    background-image:url(../images/bg-ourservices-mobile.png)
+  }
+}
+.swiper-slide img {
+  width: 100%;
+  box-shadow: inset;
+  box-shadow: 0px -200px 200px -30% rgba(0, 0, 0, 0.33) inset;
+  height: 100%;
+  object-fit: cover;
+}
+.swiper-slide .img-shadow {
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 81.67%);
+  height: 50%;
+  width: 100%;
+  position: absolute;
+  bottom: 0
+}
+.service-item-swiper {
+  position: relative;
+}
+.service-item-swiper > h2 {
+  position: absolute;
+  bottom: 0;
+  text-align: center;
+  padding: 30px;
+  font-size: 20px;
+  line-height: 1.3;
+  color: #84b5e9;
+  font-weight: bold;
+}
+#services-swiper-next, #services-swiper-prev {
+    background: rgba(0,0,0,.9);
+    box-shadow: 0px 2px 10px rgba(0,0,0,.25);
+    width: 30px;
+    height: 60px;
+    text-align: center;
+}
+#services-swiper-next::after, #services-swiper-prev::after {
+  font-family: swiper-icons;
+  text-transform: none!important;
+  letter-spacing: 0;
+  font-variant: initial;
+  line-height: 1;
+  color: #fff;
+  font-size: 12px;
+  font-weight: bold;
+}
+#services-swiper-prev {
+  left: 0 !important;
+}
+#services-swiper-next {
+  right: 0 !important;
+}
+.hover-button {
+  display: block;
+  text-align: center;
+  opacity: 0;
+  visibility: hidden;
+  transform: translateY(20px);
+  transition: all .3s ease-in-out;
+  font-weight: bold;
+  padding: 8px 14px;
+}
+
+.swiper-slide:hover h2 {
+    opacity: 0;
+    visibility: hidden;
+    transition: all .3s ease-in-out
+}
+
+.swiper-slide:hover .service-hover::after {
+    opacity: 1;
+    visibility: visible;
+    transition: all .3s ease-in-out
+}
+
+.swiper-slide:hover .service-hover .hover-title {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0);
+    transition: all .3s ease-in-out
+}
+
+.swiper-slide:hover .service-hover .hover-button {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0);
+    transition: all .3s ease-in-out;
+    display: block;
+}
+
+.service-hover {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+    padding: 15px;
+}
+.service-hover::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background: rgba(29,90,153,.68);
+    backdrop-filter: blur(3px);
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0;
+    visibility: hidden;
+    transition: all .3s ease-in-out
+}
+.hover-title {
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(-20px);
+    transition: all .3s ease-in-out;
+    margin-bottom: 30px;
+    font-size: 20px;
+    color: white;
+    font-weight: bold;
+}
+.hover-button a {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    background: #fff;
+    border-radius: 30px;
+    font-weight: bold;
+    font-size: 14px;
+    color: #1d5a99;
+    padding: 12px 30px
+}
+.hover-button {
+  font-size: 14px;
+  padding: 12px 30px;
+}
+</style>
